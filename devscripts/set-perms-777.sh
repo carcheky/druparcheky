13320be597693958826e4f3e042c062ce19d1d1a
carcheky <carcheky@gmail.com>
2020-03-25 02:36:09 +0100

    devscripts

--- devscripts/set-perms-777.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -f "web/sites/default/settings.php" ]; then
-  sudo echo "=> CORRIGIENDO PERMISOS, ESPERA...."
-  sudo chmod -R 777 .
-  drush fp
-  echo "=> PERMISOS CORREGIDOS"; else echo "=> DEBES ESTAR EN LA RAIZ DE DRUPAL";
-fi
