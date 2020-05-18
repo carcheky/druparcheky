329731de3040e82a2ad2f6fcd75bee7df79ea074
carcheky <carcheky@gmail.com>
2020-05-18 17:29:09 +0200

    composer scripts

--- scripts/recomposer.sh
+++ scripts/recomposer.sh
@@ -3,7 +3,6 @@
 composer=$(which composer)
 drush=$(which drush)
 
-
 if [ -f "web/sites/default/settings.php" ]; then
   rm -fr vendor
   rm -fr composer.lock
@@ -12,18 +11,4 @@ if [ -f "web/sites/default/settings.php" ]; then
   rm -fr web/themes/contrib
   rm -fr web/profiles/contrib
   $composer update --with-dependencies
-  cd web
-  $drush fp
-  cd ..
-fi
-
-if [ -f "sites/default/settings.php" ]; then
-  rm -fr ../vendor
-  rm -fr ../composer.lock
-  rm -fr core
-  rm -fr modules/contrib
-  rm -fr themes/contrib
-  rm -fr profiles/contrib
-  $composer update --with-dependencies
-  $drush fp
 fi
