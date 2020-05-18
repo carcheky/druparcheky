98a0c1644364395b3e61b785a9ea18b1c2d287e1
carcheky <carcheky@gmail.com>
2020-05-18 17:15:27 +0200

    scripts

--- scripts/composer-post-install.sh
+++ scripts/composer-post-install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-echo "====> composer-post-install"
+echo "====> composer-pre-install"
+
 
 if [ -d "web/sites/default" ]; then
   chmod 555 web/sites/default
+  chmod 444 web/sites/default/settings*
 fi
