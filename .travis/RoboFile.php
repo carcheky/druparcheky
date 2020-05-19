6c9b0b1419ee2bf905b9d54e3442a060c9be672a
carcheky <carcheky@gmail.com>
2020-05-19 02:48:40 +0200

    robofile

--- .travis/RoboFile.php
+++ .travis/RoboFile.php
@@ -60,10 +60,12 @@ class RoboFile extends \Robo\Tasks
         return $this->taskExecStack()
             ->stopOnFail()
             ->exec('vendor/bin/phpcs --config-set installed_paths vendor/drupal/coder/coder_sniffer')
-            ->exec('vendor/bin/phpcs --standard=Drupal web/themes/contrib')
-            ->exec('vendor/bin/phpcs --standard=DrupalPractice web/themes/contrib')
-            ->exec('vendor/bin/phpcs --standard=Drupal web/profiles/contrib')
-            ->exec('vendor/bin/phpcs --standard=DrupalPractice web/profiles/contrib')
+            ->exec('vendor/bin/phpcs --standard=Drupal web/themes/contrib/druparcheky_theme')
+            ->exec('vendor/bin/phpcs --standard=DrupalPractice web/themes/contrib/druparcheky_theme')
+            ->exec('vendor/bin/phpcs --standard=Drupal web/profiles/contrib/druparcheky/profile')
+            ->exec('vendor/bin/phpcs --standard=DrupalPractice web/profiles/contrib/druparcheky/profile')
+            ->exec('vendor/bin/phpcs --standard=Drupal web/modules/custom')
+            ->exec('vendor/bin/phpcs --standard=DrupalPractice web/modules/custom')
             ->run();
     }
 
